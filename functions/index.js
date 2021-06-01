5d43939c85156663b0c843c0086eed0925c48c16
EspiraMarvin <espiramarvin@gmail.com>
2021-06-01 17:35:52 +0300

    added callable function

--- functions/index.js
+++ functions/index.js
@@ -9,8 +9,19 @@ const functions = require("firebase-functions");
 // });
 
 //  http request 1
-
 exports.randomNumber = functions.https.onRequest((req, res) => {
   const number = Math.round(Math.random() * 100);
+  console.log(number);
   res.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((req, res) => {
+  res.redirect("https://kscore.netlify.app");
+});
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+  const name = data.name;
+  return `hello, ${name}`;
 });
